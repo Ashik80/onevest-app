36f3762aea9771276764ae894c9ea21120a3f812
Ashikur Rahman <ashikurrahman020995@gmail.com>
2022-07-14 20:58:34 +0600

    Use onevest token to make request

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const PARTNER_USER_ID = 2;
   const PRIMARY_EMAIL = 'ashik@manzil.ca';
 
-  const getOnevestJwtToken = async () => {
-     const URL = `https://webhook-gateway-service.staging.onewealth.io/tokens/generate-staging-jwt?partnerUserId=${PARTNER_USER_ID}&primaryEmail=${PRIMARY_EMAIL}`;
+  const [html, setHtml] = useState('');
+  
+  const getOnevestHtml = (html) => {
+    return {__html: html};
+  };
 
-     const response = await fetch(URL);
-     console.log(response);
-    //  const data = await response.json();
-    //  console.log(data);
+  const getOnevestPartner = async (token) => {
+    const URL = 'https://webhook-gateway-service.staging.onewealth.io/partner/full';
+
+    const config = {
+      'Content-Type': 'text/html; charset=utf-8',
+      headers: { 
+        'partner-authorization': token
+      }
+    }
+
+    const response = await fetch(URL, config)
+
+    console.log(response);
+
+    const data = await response.text();
+    console.log(data);
+    setHtml(data);
+  };
+
+  const getOnevestJwtToken = async () => {
+    const URL = `https://cors-anywhere.herokuapp.com/https://webhook-gateway-service.staging.onewealth.io/tokens/generate-staging-jwt?partnerUserId=${PARTNER_USER_ID}&primaryEmail=${PRIMARY_EMAIL}`;
+
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      if (data) {
+        await getOnevestPartner(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -21,6 +51,10 @@ function App() {
   return (
     <div className="App">
       Onevest
+      {html &&
+        <iframe title='full' sandbox width="100%"
+        height="100%" dangerouslySetInnerHTML={getOnevestHtml(html)}></iframe>
+      }
     </div>
   );
 }
